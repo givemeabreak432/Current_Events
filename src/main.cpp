6f9f67b83c14cab3070591f9d49a99b639e82cf1
Justin Kelly <justinkelly1995@hotmail.com>
2017-11-02 23:02:48 -0600

    current sensing

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@
 //SEND
 
 #define READPIN 26
+double mVperAmp = 185; // use 100 for 20A Module and 66 for 30A Module
+double RawValue= 0;
+double ACSoffset = 2500;
+double Voltage = 0;
+double Amps = 0;
 
 void setup() {
 
@@ -18,8 +23,11 @@ Serial.println("SerialTest");
 
 void loop()
 {
-  Serial.print(analogRead(READPIN));
-  Serial.print("\n");
+  RawValue = analogRead(READPIN);
+  Voltage = (RawValue / 1024.0) * 5000; // Gets you mV
+  Amps = ((Voltage - ACSoffset) / mVperAmp);
 
+  Serial.print("Amps = "); // shows the voltage measured
+  Serial.println(Amps,2); // the '2' after voltage allows you to display 2 digits after decimal point
   delay(1000);
 }
